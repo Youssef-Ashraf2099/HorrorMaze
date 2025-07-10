f2b4b3843a921609e5a7ff24a2b5c4c66b86fdf8
Mohammed Galal <62944223+Galileo204@users.noreply.github.com>
2025-07-10 03:10:09 +0300

    Loading system
    I made a loading system that simply connects a button to a scene I want to load into, how it works? I just simply connect the slider progressvalue to typecasted variable that converts the progress of the scene load into a math variable between 0 and 0.9f which will then be assigned to the progress bar real time, the loading system is asynchronous which means that the scene is loaded in the background before actually playing in the scene

--- Assets/Scripts/AsyncLoader.cs
+++ Assets/Scripts/AsyncLoader.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AsyncLoader : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Menu Screens")]
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject MainMenu;
+    [Header("Slider")]
+    [SerializeField] private Slider loadingSlider;
+
+    public void LoadLevelBtn(string levelToLoad)
     {
-        
+        MainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+
+        StartCoroutine(LoadLevelAsync(levelToLoad));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadLevelAsync(string levelToLoad)
     {
-        
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
+
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingSlider.value = progressValue;
+            yield return null;
+        }
     }
 }
